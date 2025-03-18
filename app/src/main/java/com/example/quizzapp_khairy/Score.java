5b9a903670c6d4fd96ea655a18bb0b437471f713
Sami-Khairy <samikhairy2003@gmail.com>
2025-03-18 22:30:01 +0000

    calculat final score

--- app/src/main/java/com/example/quizzapp_khairy/Score.java
+++ app/src/main/java/com/example/quizzapp_khairy/Score.java
@@ -1,18 +1,34 @@
 package com.example.quizzapp_khairy;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.progressindicator.CircularProgressIndicator;
+
 
 public class Score extends AppCompatActivity {
+
+    int resultat;
+    TextView tvScore;
+    CircularProgressIndicator progress_circular;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_score);
+
+        resultat = getIntent().getIntExtra("resultat",0);
+
+        tvScore = findViewById(R.id.tvScore);
+        progress_circular = findViewById(R.id.progress_circular);
+
+        progress_circular.setProgress(resultat);
+        tvScore.setText(resultat+"%");
 
     }
 }
