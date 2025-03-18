bb10f2f88b18ef09c5acb6c6d7bddec66f313eba
Sami-Khairy <samikhairy2003@gmail.com>
2025-03-18 22:35:57 +0000

    V-0.1

--- app/src/main/java/com/example/quizzapp_khairy/Score.java
+++ app/src/main/java/com/example/quizzapp_khairy/Score.java
@@ -1,6 +1,9 @@
 package com.example.quizzapp_khairy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,6 +17,7 @@ public class Score extends AppCompatActivity {
     int resultat;
     TextView tvScore;
     CircularProgressIndicator progress_circular;
+    Button bLogout, bTryAgain;
 
 
     @Override
@@ -25,10 +29,27 @@ public class Score extends AppCompatActivity {
         resultat = getIntent().getIntExtra("resultat",0);
 
         tvScore = findViewById(R.id.tvScore);
+        bLogout = findViewById(R.id.bLogout);
+        bTryAgain = findViewById(R.id.bTryAgain);
         progress_circular = findViewById(R.id.progress_circular);
 
         progress_circular.setProgress(resultat);
         tvScore.setText(resultat+"%");
+
+
+        bLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Score.this, MainActivity.class));
+            }
+        });
+
+        bTryAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Score.this, Quiz1.class));
+            }
+        });
 
     }
 }
